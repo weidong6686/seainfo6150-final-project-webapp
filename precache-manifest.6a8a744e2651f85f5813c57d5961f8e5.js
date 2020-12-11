d427a23da8d764c6869076eba70b8a8c62daef1c
weidong6686 <weidong6686@gmail.com>
2020-12-10 16:16:53 -0800

    Updates

---
+++ precache-manifest.6a8a744e2651f85f5813c57d5961f8e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "388824265264ef761c6cad7433e2140f",
+    "url": "/seainfo6150-final-project-webapp/index.html"
+  },
+  {
+    "revision": "baa266542cf8d498aa76",
+    "url": "/seainfo6150-final-project-webapp/static/css/main.3cd3874d.chunk.css"
+  },
+  {
+    "revision": "cbbffbd6eb110d0c1f16",
+    "url": "/seainfo6150-final-project-webapp/static/js/2.2c0bb701.chunk.js"
+  },
+  {
+    "revision": "baa266542cf8d498aa76",
+    "url": "/seainfo6150-final-project-webapp/static/js/main.3d793170.chunk.js"
+  },
+  {
+    "revision": "940ec4a42937c37ae625",
+    "url": "/seainfo6150-final-project-webapp/static/js/runtime~main.c4d7f5e8.js"
+  }
+]);
